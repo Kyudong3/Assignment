f6f5e42a43f6e4de45208aa7e1cfb396eecacead
Kyudong3 <kyudong3@gmail.com>
2019-11-05 17:48:38 +0900

    add Glide Module

---
+++ app/src/main/java/com/kyudong/assignment/module/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.kyudong.assignment.module
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule : AppGlideModule()
